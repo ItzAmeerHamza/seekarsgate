9d5fce7858fd922f192b9cc6544d737e702dc83b
ItzAmeerHamza <ameer.hamza.nu@gmail.com>
2023-08-07 01:42:54 +0500

    Added authetication

---
+++ src/Components/Admin/Teachers.js
@@ -0,0 +1,96 @@
+import React,{useState} from 'react'
+import Navbar from './Navbar'
+import { Paper, Table, TableHead,Typography,Modal, TableContainer, TableRow, TableCell, TableBody, Button } from '@mui/material';
+import { Container } from '@mui/system';
+import Footer from '../Footer';
+const Teachers = () => {
+    const ourCourses = [{
+        catogory:"Computer Science",
+        name:"John Smith",
+        releasedDate:"20-5-2023",
+        experience:"2 years",
+        email:"johmSmith@gmail.com"
+    },{
+        catogory:"Psychology",
+        name:"Adam north",
+        releasedDate:"26-8-2023",
+        experience:"3 years",
+        email:"adamnorth@gmail.com"
+    },{
+        catogory:"Computer Science",
+        name:"John Smith",
+        releasedDate:"20-5-2023",
+        experience:"2 years",
+        email:"johmSmith@gmail.com"
+    },{
+        catogory:"Psychology",
+        name:"Adam north",
+        releasedDate:"26-8-2023",
+        experience:"3 years",
+        email:"adamnorth@gmail.com"
+    },{
+        catogory:"Computer Science",
+        name:"John Smith",
+        releasedDate:"20-5-2023",
+        experience:"2 years",
+        email:"johmSmith@gmail.com"
+    },{
+        catogory:"Psychology",
+        name:"Adam north",
+        releasedDate:"26-8-2023",
+        experience:"3 years",
+        email:"adamnorth@gmail.com"
+    },{
+        catogory:"Computer Science",
+        name:"John Smith",
+        releasedDate:"20-5-2023",
+        experience:"2 years",
+        email:"johmSmith@gmail.com"
+    }]
+  return (
+    <>
+    <Navbar/>
+    <Container maxWidth="lg" sx={{ textAlign: "center",mt:"20px" }}>
+       <br/>
+        
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead sx={{ background: "#1769aa" }}>
+                                <TableRow>
+                                    <TableCell><Typography color="white" variant="h6"><b>Catogory</b></Typography></TableCell>
+                                    <TableCell><Typography color="white" variant="h6"><b>Name</b></Typography></TableCell>
+                                    <TableCell><Typography color="white" variant="h6"><b>Joining Date</b></Typography></TableCell>
+                                    <TableCell><Typography color="white" variant="h6"><b>Experience</b></Typography></TableCell>
+                                    <TableCell><Typography color="white" variant="h6"><b>Email</b></Typography></TableCell>
+                                    <TableCell><Typography color="white" variant="h6"><b>Remove Teacher</b></Typography></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+
+                                {ourCourses.map((p) => {
+                                    return (
+                                        <>
+                                            <TableRow>
+                                                <TableCell><Typography>{p.catogory}</Typography></TableCell>
+                                                <TableCell><Typography>{p.name}</Typography></TableCell>
+                                                <TableCell><Typography>{p.releasedDate}</Typography></TableCell>
+                                                <TableCell><Typography>{p.experience}</Typography></TableCell>
+                                                <TableCell><Typography>{p.email}</Typography></TableCell>
+                                                <TableCell><Typography><Button variant="contained" sx={{ background: "red" }}>Delete</Button></Typography></TableCell>
+                                            </TableRow>
+                                        </>
+                                    )
+                                })}
+
+                            </TableBody>
+
+                        </Table>
+                    </TableContainer><br />
+
+                </Container><br/><br/>
+                <Footer/>
+    </>
+  )
+}
+
+export default Teachers
